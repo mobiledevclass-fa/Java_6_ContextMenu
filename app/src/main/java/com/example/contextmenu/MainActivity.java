816075e8d136d9330e5c125584def778f46513ac
mvkoroteev <mvkoroteev@fa.ru>
2019-03-27 11:26:36 +0300

    Code added

--- app/src/main/java/com/example/contextmenu/MainActivity.java
+++ app/src/main/java/com/example/contextmenu/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.contextmenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvColor, tvSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvColor = (TextView) findViewById(R.id.tvColor);
+        tvSize = (TextView) findViewById(R.id.tvSize);
+
+        // для tvColor и tvSize необходимо создавать контекстное меню
+        registerForContextMenu(tvColor);
+        registerForContextMenu(tvSize);
     }
 }
